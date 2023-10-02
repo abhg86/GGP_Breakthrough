c35cf0e0a17690eb270f445aa798dd5a7a2b4258
abhg86 <aymericbenzou@gmail.com>
2023-10-02 12:12:42 +0200

    Run normal Breakthrough

---
+++ src/main/java/run/RunAI.java
@@ -0,0 +1,98 @@
+package run;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import game.Game;
+import AIs.ExampleUCT;
+import other.AI;
+import other.GameLoader;
+import other.context.Context;
+import other.model.Model;
+import other.trial.Trial;
+
+
+
+public class RunAI
+{
+	public static void main(final String[] args)
+	{
+		for (int i=0; i<Integer.valueOf(args[1]); i++)
+		{
+
+			Game game = GameLoader.loadGameFromName("Breakthrough.lud");
+			
+			Trial trial = new Trial(game);
+			Context context = new Context(game, trial);
+			game.start(context);
+			
+			System.out.println("We're playing " + game.name() + "!");
+			
+			// Create and init two agents
+			final List<AI> ais = new ArrayList<AI>(3);
+			ais.add(null);
+			for (int j=1; j<=game.players().size(); j++){
+				ais.add(new ExampleUCT());
+				ais.get(j).initAI(game, j);
+			}
+			
+			// This model object is the thing that will handle control flow for us
+			final Model model = context.model();
+			
+			// Keep going until the game is over...
+			while (!trial.over())
+			{
+				// The following call tells the model it should start a new "step"
+				// using the given list of AIs, with 0.2 seconds of thinking time
+				// per decision, per agent.
+				//
+				// The behaviour of this call depends on whether the model is for
+				// alternating-move games or simultaneous-move games, but the basic
+				// premise is that whatever AI(s) is (are) supposed to make a move
+				// will start thinking about its move, and apply it to the game
+				// state once the decision has been made.
+				//
+				// In an alternating-move game, this means a single agent is
+				// queried to return a move, and that move is applied. In a
+				// simultaneous-move game, it means that ALL active players are
+				// queried to return moves, and they are all applied together.
+				model.startNewStep(context, ais, 0.2);
+				
+				// In the following loop, we wait around until the model tells
+				// us that it's ready with the processing of the step we asked
+				// it to start.
+				//
+				// Generally this wouldn't be needed, because the startNewStep()
+				// call should block and only return once any relevant AIs have
+				// selected and applied their move to the game state. However,
+				// the behaviour of the startNewStep() can be modified in various
+				// ways by adding additional arguments. One possible modification
+				// is to have it return immediately (which implies that any
+				// AI-thinking must be performed in a separate Thread). A loop
+				// like the one below may then, for example, be used for 
+				// visualisation of the AI's thinking process, or for processing
+				// human-player input in case any of the AIs were set to null.
+				while (!model.isReady())
+				{
+					try
+					{
+						Thread.sleep(100);
+					}
+					catch (final InterruptedException e)
+					{
+						e.printStackTrace();
+					}
+				}
+				
+				// There is no need to explicitly apply any moves here anymore;
+				// if model.isReady() returns true, this means we're ready for
+				// the next time step!
+			}
+			
+			// let's see what the result is
+			System.out.println(context.trial().status());
+		}
+	}
+}
+
+
